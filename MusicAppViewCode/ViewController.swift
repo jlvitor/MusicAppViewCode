6082ef2622d5a47443c31f4b9d4f6d2b2aea1bf6
Jean Lucas Vitor <65193042+jlvitor@users.noreply.github.com>
2022-04-18 03:52:00 -0300

    First commit

---
+++ MusicAppViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAppViewCode
+//
+//  Created by Jean Lucas Vitor on 18/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
